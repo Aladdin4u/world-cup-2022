92c38c354aa95b27800522ec7e0e37a526a3f998
aladdin4u <taofikakinkumi@gmail.com>
2022-11-25 09:35:41 +0100

    fix images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
     return ( 
         <>
             <div>
-                <img src="/wcimage/fifa2022.jpg" style={{width:"100%",height:"auto"}}  alt="fifa2022" />
+                <img src="https://user-images.githubusercontent.com/101972392/203936641-33329426-af72-42d0-8604-89a4898fb023.jpg" style={{width:"100%",height:"auto"}}  alt="fifa2022" />
             </div>
             <Live />
             <div>
-                <img src="/wcimage/Groups.png" style={{width:"100%",height:"auto"}}  alt="Group" />
+                <img src="https://user-images.githubusercontent.com/101972392/203936706-8975ea9f-74a7-4437-ba34-6d2ab64d98f0.png" style={{width:"100%",height:"auto"}}  alt="Group" />
             </div>
         </>
      );
