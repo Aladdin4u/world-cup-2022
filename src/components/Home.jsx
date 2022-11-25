eb72e6318a16ef3432d1bd3b8b79f21b7f8181bb
aladdin4u <taofikakinkumi@gmail.com>
2022-11-25 08:58:46 +0100

    update scores

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return ( 
         <>
             <div>
-                <img src="/wcimage/e08a63142069201.625ffbdb37740.jpg" style={{width:"100%",height:"auto"}}  alt="fifa2022" />
+                <img src="/wcimage/fifa2022.jpg" style={{width:"100%",height:"auto"}}  alt="fifa2022" />
             </div>
             <Live />
             <div>
