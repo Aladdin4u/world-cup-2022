4c3c18189774f0e6b5bb7cec9d281cf14eee0dbd
aladdin4u <taofikakinkumi@gmail.com>
2022-11-25 15:41:15 +0100

    changes

--- src/components/Live.jsx
+++ src/components/Live.jsx
@@ -4,28 +4,39 @@ import match from '../data/match.js'
 const Fixtures = (props) => {
 
     return (
-        <div>
-            <div className='fixture-list' style={{marginBottom:"2px"}}>
-                <div className='fixture-title'>
-                    <img src={props.homeImg} alt="team" className="box-img"/>
-                    <span>{props.homeName}</span>
+        <>
+            <div className='live-list'>
+                <h4 className='live-title' style={{margin:0}}>
+                    Group {props.grpName}
+                </h4>
+                <div className="live-row">
+                    <div className="live-column">
+                        <div className='live-row-list'>
+                            <img src={props.homeImg} alt="team" className="box-img"/>
+                            <span className="span-right">{props.homeName}</span>
+                            <span >{props.homeScore}</span>
+                        </div>
+                        <div className='live-row-list'>
+                            <img src={props.awayImg} alt="team" className="box-img"/>
+                            <span className="span-right">{props.awayName}</span>
+                            <span>{props.awayScore}</span>
+                        </div>
+                    </div>
+                    <div className="live-right">
+                        {props.time}
+                    </div>
                 </div>
-                <div className='score'>{props.homeScore}</div>
-                <div className='score' style={{marginLeft:0}}>{props.awayScore}</div>
-                <div className='fixture-title'>
-                    <img src={props.awayImg} alt="team" className="box-img"/>
-                    <span>{props.awayName}</span>
-                </div>
+                
             </div>
-            <div className="team-Scorers" >
+            {/* <div className="team-Scorers" >
                 <div className='team-Scorer'>
                     {props.playerHome}
                 </div>
                 <div className='team-Scorer'>
                     {props.playerAway}
-                </div>
-            </div>
-        </div>
+                </div> */}
+            {/* </div> */}
+        </>
     );
 }
 
@@ -53,14 +64,18 @@ const Live = () => {
     const matchElement = matches.filter(match => match.matchday == dates).map(match => (
         <Fixtures 
         key={match._id}
-        homeImg={match.home_flag}
+        grpName={match.group}
+        homeImg="wcimage/Spain.png"
+        // {match.home_flag}
         homeName={match.home_team_en}
         homeScore={match.time_elapsed === "notstarted" ? "-" : match.home_score}
         awayScore={match.time_elapsed === "notstarted" ? "-" : match.away_score}
-        awayImg={match.away_flag}
+        awayImg="wcimage/Ghana.png"
+        // {match.away_flag}
         awayName={match.away_team_en}
         playerHome={match.time_elapsed === "notstarted" ? "" : match.home_scorers} 
         playerAway={match.time_elapsed === "notstarted" ? "" : match.away_scorers}
+        time={match.time_elapsed === "notstarted" ? match.local_date : "finished"}
     />
     ))
 
@@ -73,7 +88,7 @@ const Live = () => {
             <div className="rowLive" style={{justifyContent:"space-between", alignItems: "center"}}>
                 <span onClick={subMatchDay}><svg style={{width: 20, height: 20,color:"white",}} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path></svg></span>
 
-                <h3 style={{color:"white",}}>MatchDay {dates}</h3>
+                <h3 style={{color:"white",}}>MatchDay {dates} {matchElement.time}</h3>
 
                 <span onClick={addMatchDay}><svg style={{width: 20, height: 20, color:"white",}} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path></svg></span>
             </div>
