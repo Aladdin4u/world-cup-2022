6f1e4f84afcc2c63539ea8614aedde0af98155b3
aladdin4u <taofikakinkumi@gmail.com>
2022-12-04 16:00:24 +0100

    update scores

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Live from "./Live";
+import Round from "./Round";
 
 const Home = () => {
     return ( 
@@ -7,7 +8,11 @@ const Home = () => {
             <div>
                 <img src="https://user-images.githubusercontent.com/101972392/203936641-33329426-af72-42d0-8604-89a4898fb023.jpg" style={{width:"100%",height:"auto"}}  alt="fifa2022" />
             </div>
-            <Live />
+            <Round group="R16" type="Round 16" />
+            <Round group="QR" type="Quarter Final" />
+            <Round group="semi" type="Semi Final" />
+            <Round group="3RD" type="Third Place" />
+            <Round group="FIN" type="Final" />
             <div>
                 <img src="https://user-images.githubusercontent.com/101972392/203936706-8975ea9f-74a7-4437-ba34-6d2ab64d98f0.png" style={{width:"100%",height:"auto"}}  alt="Group" />
             </div>
--- src/components/Round.jsx
+++ src/components/Round.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import match from '../data/match.js'
 
 const Fixtures = (props) => {
-
+    let dateArray = props.date
+    function splitLocalDate(date) {
+        return date.split(" ")[0]
+    }
+    let date = splitLocalDate(dateArray)
     return (
         <>
             <div className='live-list'>
                 <h4 className='live-title' style={{margin:0}}>
-                    Group {props.grpName}
+                    {date}
                 </h4>
                 <div className="live-row">
                     <div className="live-column">
@@ -28,43 +32,18 @@ const Fixtures = (props) => {
                 </div>
                 
             </div>
-            {/* <div className="team-Scorers" >
-                <div className='team-Scorer'>
-                    {props.playerHome}
-                </div>
-                <div className='team-Scorer'>
-                    {props.playerAway}
-                </div> */}
-            {/* </div> */}
         </>
     );
 }
 
-const Live = (props) => {
+const Round = (props) => {
     const [matches, setMatches] = useState(match[0].data)
-    const [dates, setDates] = useState(13)
-    const [hide, setHide] = useState(true)
 
-    function addMatchDay() {
-        if(dates < 13) {
-            setDates(prevDate => prevDate + 1)
-        }
-    }
-    function subMatchDay() {
-        if(dates > 1) {
-            setDates(prevDate => prevDate - 1)
-        }
-    }
-
-    function handleHide() { 
-        setHide(prevHide => !prevHide)
-    }
-
-    let matchDay = ""
-    const matchElement = matches.filter(match => match.matchday == dates).map(match => (
+    let group = props.group
+    const matchElement = matches.filter(match => match.group == group).map(match => (
         <Fixtures 
         key={match._id}
-        grpName={match.group}
+        date={match.local_date}
         homeImg={match.home_flag}
         homeName={match.home_team_en}
         homeScore={match.time_elapsed === "notstarted" ? "-" : match.home_score}
@@ -76,26 +55,15 @@ const Live = (props) => {
         time={match.time_elapsed === "notstarted" ? match.local_date : match.time_elapsed === "h1" ? "h1" : match.time_elapsed === "h2" ? "h2" : "finished"}
     />
     ))
-
-    const center = {
-
-    }
-
+    
     return ( 
-        <section className="" style={{marginTop:"70px"}}>
-            <div className="rowLive" style={{justifyContent:"space-between", alignItems: "center"}}>
-                <span onClick={subMatchDay}><svg style={{width: 20, height: 20,color:"white",}} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path></svg></span>
-
-                <h3 style={{color:"white",}}>MatchDay {dates} {matchElement.time}</h3>
-
-                <span onClick={addMatchDay}><svg style={{width: 20, height: 20, color:"white",}} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path></svg></span>
-            </div>
+        <section>
             <div className="column">
-                <h1 style={{color:"white",}}>Live games</h1>
+                <h1 style={{color:"white",}}>{props.type}</h1>
                 {matchElement}
             </div>
         </section>
      );
 }
  
-export default Live;
+export default Round;
